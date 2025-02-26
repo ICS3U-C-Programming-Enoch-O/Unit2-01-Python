35213850696f966f5f55f4c14258226044213cee
Enoch Oyeniyi <enoch.oyeniyi@stu.ocsb.ca>
2025-02-26 17:15:02 +0000

    area_per_rect_input.py

---
+++ “area_per_rect_input.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+# Created By: Enoch Oyeniyi
+# Date: February 26, 2025
+# This program displays Hello, World! with proper style
+
+length = int(input("Enter length of the rectangle (30): "))
